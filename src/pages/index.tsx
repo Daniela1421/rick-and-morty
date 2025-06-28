e3b16a0b569bc56acf433a45adc41a36bf71cb57
daniela1421 <daniela.ducuara1817@gmail.com>
2025-06-27 19:15:32 -0500

    feat: Add a main page with filters, status handling, and dynamic messages for loading, errors, and results.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,68 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { useEffect, useState } from "react";
+import { useCharacters } from "@/hooks/useCharacters";
+import { CharacterGrid } from "@/components/CharacterGrid";
+import { FilterSidebar } from "@/components/FilterSideBar";
 
 export default function Home() {
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+  const [gender, setGender] = useState('');
+  const [hasSearched, setHasSearched] = useState(false);
+  const { characters, loading, error } = useCharacters(name, status, gender); 
+
+  useEffect(() => {
+    if (name || status || gender) {
+      setHasSearched(true);
+    }
+  }, [name, status, gender]);
+
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="min-h-screen bg-gray-50 text-gray-800">
+      <main className="max-w-7xl mx-auto px-4 py-8">
+        <h1 className="text-4xl font-extrabold text-center text-lime-600 mb-10">
+          Personajes de Rick & Morty
+        </h1>
+        <div className="flex flex-col sm:flex-row gap-8">
+          <aside className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <FilterSidebar
+              name={name}
+              status={status}
+              gender={gender}
+              onNameChange={setName}
+              onStatusChange={setStatus}
+              onGenderChange={setGender}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </aside>
+          <section className="w-full md:w-2/3 lg:w-3/4">
+            {loading && (
+              <div className="flex justify-center items-center">
+                <div className="bg-lime-100 text-lime-800 px-6 py-4 rounded-md shadow text-center w-full">
+                  Cargando personajes...
+                </div>
+              </div>
+            )}
+            {error && (
+              <div className="flex justify-center items-center">
+                <div className="bg-red-100 text-red-700 px-6 py-4 rounded-md shadow text-center w-full">
+                  {error}
+                </div>
+              </div>
+            )}
+            {!loading && !error && characters.length > 0 && (
+              <CharacterGrid characters={characters} />
+            )}
+            {!loading && !error && characters.length === 0 &&  hasSearched && (
+              <div className="flex justify-center items-center">
+                <div className="bg-yellow-100 text-yellow-800 px-6 py-4 rounded-md shadow text-center w-full">
+                  No se encontraron personajes con los filtros aplicados.
+                </div>
+              </div>
+            )}
+          </section>
         </div>
+
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
+
